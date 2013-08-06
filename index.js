ceefdc3d3e9d0e17600f471a8768db3b7032580e
David Dias <daviddias.p@gmail.com>
2013-08-06 22:27:37 +0100

    Initial Import

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var request = require('request')
-  , inspect = require('util').inspect
 
 var validator = function (email, cb) {  
   var options = {
       url: 'https://api.mailgun.net/v2/address/validate'
     , method: 'GET'
-    , qs: {address: email}
+    , qs: {address: email}//, syntax_only: true}
     , auth: {
           username: "api"
         , password: "pubkey-5ogiflzbnjrljiky49qxsiozqef5jxp7"
       }
   }
   request(options, function (err, result){
-    cb(err, result.request.response.body)    
+    if(err) {cb(err)}
+    //this body is not a real JSON object  
+    if(result.request.response.body.split(":")[1].split(",")[0].replace(" ","") === 'false'){
+      cb(err, false)    
+    } else {cb(err, true)}  
   })
 }
+
 module.exports = validator
 
 
